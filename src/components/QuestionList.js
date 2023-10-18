4748a8808c47395882f4ee965deee8bd6b9cd2ad
saiderrahmani <didi.mousedidi@outlook.com>
2023-10-18 01:53:43 -0400

    fixt questionlist

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,28 @@
-import React from "react";
+ 
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem"; 
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    // Fetch questions from the API when the component mounts
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => {
+        setQuestions(data);
+      })
+      .catch((error) => console.error("Error fetching questions: ", error));
+  }, []); // The empty dependency array ensures this effect runs once when the component mounts
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem key={question.id} question={question} />
+        ))}
+      </ul>
     </section>
   );
 }
